33d193f6eb6b48b18f2cfe9495f0e4a526dc1f1f
leo-ishere <110486918+leo-ishere@users.noreply.github.com>
2026-06-30 16:11:06 +0100

    add implementation of default command in client.js

--- src/client.js
+++ src/client.js
@@ -1,54 +1,40 @@
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const registerCommands = require('./registry/registerCommands');
 const getLocalCommands = require('./registry/getLocalCommands');
+const getBuiltInCommands = require('./registry/getBuiltInCommands');
 const { loadEvents } = require('./handlers/eventHandler');
 const { handleCommand } = require('./handlers/commandHandler');
 
-const defaultIntents = [
-  GatewayIntentBits.Guilds,
-  GatewayIntentBits.GuildMembers,
-  GatewayIntentBits.GuildMessages,
-  GatewayIntentBits.MessageContent,
-  GatewayIntentBits.DirectMessages,
-];
-
-const defaultPartials = [
-  Partials.Message,
-  Partials.Channel,
-  Partials.Reaction,
-];
+// ... defaultIntents, defaultPartials unchanged ...
 
 class FrameworkClient extends Client {
   constructor(options = {}) {
-    const {
-      intents = defaultIntents,
-      partials = defaultPartials,
-      commandsPath,
-      eventsPath,
-      devUserIds = [],
-      ...clientOptions
-    } = options;
-
-    super({ intents, partials, ...clientOptions });
-
-    this.config = {
-      commandsPath,
-      eventsPath,
-      devUserIds,
-    };
-
-    this.commands = new Map();
+    // ... constructor unchanged ...
   }
 
   async start(token) {
     console.log('BOOT | Preparing to login...');
     const time = Date.now();
 
+    const builtInCommands = getBuiltInCommands();
+
     const localCommands = getLocalCommands(this.config.commandsPath);
+    
+    const allCommands = [...builtInCommands];
     for (const cmd of localCommands) {
+      const existingIndex = allCommands.findIndex(c => c.name === cmd.name);
+      if (existingIndex !== -1) {
+        allCommands[existingIndex] = cmd; // override with bot's version
+      } else {
+        allCommands.push(cmd);
+      }
+    }
+
+    this.commands.clear();
+    for (const cmd of allCommands) {
       this.commands.set(cmd.name, cmd);
     }
-    
+
     if (this.config.eventsPath) {
       await loadEvents(this, this.config.eventsPath);
     }
@@ -63,10 +49,10 @@ class FrameworkClient extends Client {
 
     await new Promise((resolve) => {
       if (this.isReady()) return resolve();
-      this.once('clientReady', resolve);
+      this.once('clientReady', resolve);   // fixed deprecation
     });
 
-    await registerCommands(this, localCommands);
+    await registerCommands(this, allCommands);
 
     const timeTaken = Date.now() - time;
     console.log(`BOOT | ${this.user.username} is online (took ${timeTaken}ms)`);
